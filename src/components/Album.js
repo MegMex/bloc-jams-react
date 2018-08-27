54f6e9ed679bf7c66c135eb75d1f7262e17b06f7
Meg <meggo@gmail.com>
2018-08-26 20:07:11 -0600

    Checkpoint 6: components album complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,32 +2,49 @@ import React, { Component } from 'react';
 import albumData from './../data/albums';
 
 class Album extends Component {
-   constructor(props) {
-     super(props);
-   }
+    constructor(props) {
+        super(props);
 
-      const album = albumData.find( album => {
-        return album.slug === this.props.match.params.slug
-      });
+        const album = albumData.find( album => {
+            return album.slug === this.props.match.params.slug
+        });
 
-      this.state = {
-        album: album
-      };
+        this.state = {
+            album: album
+        };
+    }
 
-   render() {
-     return (
-       <section className="album">
-         <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-           <div className="album-details">
-             <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>
-       </section>
-     );
-   }
- }
+    render() {
+        return (
+          <section className="album">
+            <section id="album-info">
+              <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+              <div className="album-details">
+                <h1 id="album-title">{this.state.album.title}</h1>
+                <h2 className="artist">{this.state.album.artist}</h2>
+                <div id="release-info">{this.state.album.releaseInfo}</div>
+              </div>
+            </section>
+
+            <table id="song-list">
+             <colgroup>
+              <col id="song-number-column" />
+              <col id="song-title-column" />
+              <col id="song-duration-column" />
+             </colgroup>
+             <tbody>
+               {this.state.album.songs.map( (song, index) =>
+                return (
+                  <tr key={index}>
+                      <tr>{song}</tr>
+                      <tr>{song.length} song</tr>
+                  </tr>
+                ))}
+             </tbody>
+            </table>
+          </section>
+        );
+    }
+}
 
 export default Album;
